095f0ac6df10cdb05a3c8411770b94fd8d61b458
Jain <nikitesh.jain@capgemini.com>
2016-06-23 16:51:34 +0530

    Nikitesh Mastering CI Training

---
+++ Gradle Project/src/test/java/demo/AccountTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AccountTest {
+	@Test
+	public void test(){
+		assertEquals(true, true);
+	}
+
+}
